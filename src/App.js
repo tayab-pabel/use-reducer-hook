8b63156b74584bee05a28a4a8b5043ea6ef427d9
tayab-pabel <tayabpabel@yahoo.com>
2020-09-15 22:23:42 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import PatientManagement from './components/ReducerCount/PatientManagement/PatientManagement';
+import PatientReducer from './components/ReducerCount/PatientReducer/PatientReducer';
+import ReducerCount from './components/ReducerCount/ReducerCount';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReducerCount></ReducerCount>
+      <PatientManagement></PatientManagement>
     </div>
   );
 }
---
+++ src/components/ReducerCount/ReducerCount.js
@@ -0,0 +1,29 @@
+import React, { useReducer, useState } from 'react';
+
+const initialState = {count: 0}
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return {count: state.count + 1}
+        case 'DECREMENT':
+            return {count: state.count - 1}
+        default:
+            return state;
+    }
+}
+
+const ReducerCount = () => {
+    const [count, setCount] = useState(0);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div>
+            <h1>This is reducer count: {state.count}</h1>
+            <button onClick={() => dispatch({type: 'INCREMENT'})}>Increment</button>
+            <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement</button>
+        </div>
+    );
+};
+
+export default ReducerCount;
